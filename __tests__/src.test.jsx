1bc9ba1eb702c15060d3d57186c63340da90f617
robertriosm <87707371+robertriosm@users.noreply.github.com>
2022-05-19 22:31:37 -0600

    ya casi termino

--- __tests__/src.test.jsx
+++ __tests__/src.test.jsx
@@ -1,44 +1,100 @@
-import Adapter from 'enzyme-adapter-react-16'
-// import { shallow, configure, mount } from 'enzyme'
-import { shallow, configure, render } from 'enzyme'
 import React from 'react'
 import '@testing-library/jest-dom'
-// import { screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import App from '../src/components/App'
-// import Tecla from '../src/components/Tecla'
 
-configure({ adapter: new Adapter() })
-// const tryFunction = (aValue) => {
-//   return aValue
-// }
-
-// desplegar correctamente los componentes y comprobar los returns de las funciones
-// todos, aprobados, GREEN TEST
 describe('tests para la calculadora', () => {
   // test para la suma
   it('test de suma', () => {
-    const wrapper = shallow(<App name="Roberto" />)
-    wrapper.find({ Tvalue: '1' }).simulate('click')
-    wrapper.find({ Tvalue: '0' }).simulate('click')
-    wrapper.find({ Tvalue: '+' }).simulate('click')
-    wrapper.find({ Tvalue: '1' }).simulate('click')
-    wrapper.find({ Tvalue: '0' }).simulate('click')
-    wrapper.find({ Tvalue: '=' }).simulate('click')
-    // expect(screen.queryByText('20')).toBeInTheDocument()
-    expect(wrapper.find('h3#results'))
-
-    const wrapper2 = render(<App name="Roberto" />)
-    let concat = wrapper2.find('concatenar')
-    concat.
-    // eslint-disable-next-line no-console
-    // console.log(result)
-    // expect()
-    // test para la resta
-    // test para la multiplicacion
-    // test para la division
-    // test para cambiar signo
-    // test para modulos
-    // test para la concatenacion
-    // test para la igualdad
+    render(<App name="Roberto" />)
+    fireEvent.click(document.getElementById('1'))
+    fireEvent.click(document.getElementById('0'))
+    fireEvent.click(document.getElementById('+'))
+    fireEvent.click(document.getElementById('1'))
+    fireEvent.click(document.getElementById('0'))
+    fireEvent.click(document.getElementById('='))
+    expect(screen.getByTestId('result')).toHaveTextContent('20')
+  })
+  // test para la resta
+  it('test de resta', () => {
+    render(<App name="Roberto" />)
+    fireEvent.click(document.getElementById('1'))
+    fireEvent.click(document.getElementById('0'))
+    fireEvent.click(document.getElementById('-'))
+    fireEvent.click(document.getElementById('1'))
+    fireEvent.click(document.getElementById('0'))
+    fireEvent.click(document.getElementById('='))
+    expect(screen.getByTestId('result')).toHaveTextContent('0')
+  })
+  // test para la multiplicacion
+  it('test de multiplicacion', () => {
+    render(<App name="Roberto" />)
+    fireEvent.click(document.getElementById('1'))
+    fireEvent.click(document.getElementById('0'))
+    fireEvent.click(document.getElementById('x'))
+    fireEvent.click(document.getElementById('1'))
+    fireEvent.click(document.getElementById('0'))
+    fireEvent.click(document.getElementById('='))
+    expect(screen.getByTestId('result')).toHaveTextContent('100')
+  })
+  // test para la division
+  it('test de division', () => {
+    render(<App name="Roberto" />)
+    fireEvent.click(document.getElementById('1'))
+    fireEvent.click(document.getElementById('0'))
+    fireEvent.click(document.getElementById('/'))
+    fireEvent.click(document.getElementById('0'))
+    fireEvent.click(document.getElementById('='))
+    expect(screen.getByTestId('result')).toHaveTextContent('error')
+  })
+  // test para cambiar signo
+  it('test del boton +/-', () => {
+    render(<App name="Roberto" />)
+    fireEvent.click(document.getElementById('1'))
+    fireEvent.click(document.getElementById('0'))
+    fireEvent.click(document.getElementById('+'))
+    fireEvent.click(document.getElementById('1'))
+    fireEvent.click(document.getElementById('0'))
+    fireEvent.click(document.getElementById('='))
+    fireEvent.click(document.getElementById('+/-'))
+    expect(screen.getByTestId('result')).toHaveTextContent('-20')
+    fireEvent.click(document.getElementById('+/-'))
+    expect(screen.getByTestId('result')).toHaveTextContent('20')
+  })
+  // test para modulos
+  it('test para modulos', () => {
+    render(<App name="Roberto" />)
+    fireEvent.click(document.getElementById('1'))
+    fireEvent.click(document.getElementById('0'))
+    fireEvent.click(document.getElementById('%'))
+    fireEvent.click(document.getElementById('1'))
+    fireEvent.click(document.getElementById('='))
+    expect(screen.getByTestId('result')).toHaveTextContent('0')
+  })
+  // test para la igualdad
+  it('test para verificar igualdad ==', () => {
+    render(<App name="Roberto" />)
+    fireEvent.click(document.getElementById('0'))
+    fireEvent.click(document.getElementById('=='))
+    fireEvent.click(document.getElementById('0'))
+    fireEvent.click(document.getElementById('='))
+    expect(screen.getByTestId('result')).toHaveTextContent('true')
+  })
+  // test para la concatenacion
+  it('test del limite de la concatenacion (9 digitos)', () => {
+    render(<App name="Roberto" />)
+    fireEvent.click(document.getElementById('9'))
+    fireEvent.click(document.getElementById('9'))
+    fireEvent.click(document.getElementById('9'))
+    fireEvent.click(document.getElementById('9'))
+    fireEvent.click(document.getElementById('9'))
+    fireEvent.click(document.getElementById('x'))
+    fireEvent.click(document.getElementById('9'))
+    fireEvent.click(document.getElementById('9'))
+    fireEvent.click(document.getElementById('9'))
+    fireEvent.click(document.getElementById('9'))
+    fireEvent.click(document.getElementById('9'))
+    fireEvent.click(document.getElementById('='))
+    expect(screen.getByTestId('result')).toHaveTextContent('error')
   })
 })
